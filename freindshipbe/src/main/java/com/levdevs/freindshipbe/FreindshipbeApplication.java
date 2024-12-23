c12ee5eb4fa243f9f8b19d93a5a6af00eace3abe
code613 <benmarcus724@gmail.com>
2024-12-23 14:23:59 -0500

    other files

---
+++ freindshipbe/src/main/java/com/levdevs/freindshipbe/FreindshipbeApplication.java
@@ -0,0 +1,13 @@
+package com.levdevs.freindshipbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreindshipbeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreindshipbeApplication.class, args);
+	}
+
+}
---
+++ freindshipbe/src/test/java/com/levdevs/freindshipbe/FreindshipbeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.levdevs.freindshipbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreindshipbeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
