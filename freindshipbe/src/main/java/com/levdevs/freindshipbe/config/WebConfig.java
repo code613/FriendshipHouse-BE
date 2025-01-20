63faf8fd3f08f5efb80f3ae54e50fb0454350abc
code613 <benmarcus724@gmail.com>
2025-01-20 15:47:07 -0500

    allow cors for http://localhost:3000 adding , "OPTIONS"

--- freindshipbe/src/main/java/com/levdevs/freindshipbe/config/WebConfig.java
+++ freindshipbe/src/main/java/com/levdevs/freindshipbe/config/WebConfig.java
@@ -8,7 +8,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // Allow all endpoints
                 .allowedOrigins("http://localhost:3000") // Allow specific origin (React or frontend)
-                .allowedMethods("GET", "POST", "PUT", "DELETE") // Allow specific HTTP methods
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Allow specific HTTP methods
                 .allowedHeaders("*") // Allow all headers
                 .allowCredentials(true); // Allow credentials (cookies, etc.)
     }
